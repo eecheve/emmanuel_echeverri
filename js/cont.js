24fabc54421b8c934c6207d267d1c4d2dfbe3a43
Emmanuel Echeverri <ei.echeverri2@gmail.com>
2023-03-27 07:25:07 -0400

    3.27.2023_7:23AM
    - added a few .js modules for the navigation bar and the contact footer in the landing page. (unsure if supported by github yet).
    - modified bullet list icons for them to be in color.
    - Created downloadable link to CV pdf.

---
+++ js/cont.js
@@ -0,0 +1,17 @@
+const navContent =`
+  <li style="list-style-image: url('../icons/gmail.png')"><a class="contact_item" href="mailto:ei.echeverri2@gmail.com">ei.echeverri2@gmail.com</a></li>
+  <li style="list-style-image: url('../icons/github.png')"><a class="contact_item" href="https://github.com/eecheve">GitHub</a></li>
+  <li style="list-style-image: url('../icons/orcid.png')"><a class="contact_item" href="https://orcid.org/0000-0002-5199-897X">ORCID</a></li>
+  <li style="list-style-image: url('../icons/linkedin.png')"><a class="contact_item" href="https://linkedin.com/in/emmanuel-echeverri-jimenez/">LinkedIn</a></li>
+  <li style="list-style-image: url('../icons/twitter.png')"><a class="contact_item" href="https://twitter.com/eiecheverri">Twitter</a></li>
+`;
+
+const mainNav = document.createElement("cont");
+mainNav.classList.add("contact");
+
+const navList = document.createElement("ul");
+navList.classList.add("footer_panel");
+navList.innerHTML = navContent;
+mainNav.append(navList);
+
+document.querySelector(".contact_info").append(mainNav);
---
+++ js/nav.js
@@ -0,0 +1,17 @@
+const navContent =`
+  <li><a class="btn" href="../../index.html">Home</a></li>
+  <li><a class="btn" href="../html/portfolio.html">Portfolio</a></li>
+  <li><a class="btn" href="../html/experience.html">Experience</a></li>
+  <li><a class="btn" href="../html/education.html">Education</a></li>
+  <li><a class="btn" href="../html/publications.html">Publications</a></li>
+`;
+
+const mainNav = document.createElement("nav");
+mainNav.classList.add("navigation");
+
+const navList = document.createElement("ul");
+navList.classList.add("panel");
+navList.innerHTML = navContent;
+mainNav.append(navList);
+
+document.querySelector(".nav_panel").append(mainNav);
